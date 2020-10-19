24a450918ca698149a6b620ebf4ad475435998cb
Armen Phelps <armenphelps@Armens-MacBook-Air.local>
2020-10-18 23:56:47 -0700

    added views for main pages

---
+++ Transfer Assistant App/ViewController5_feedback.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController5_feedback.swift
+//  Transfer Assistant App
+//
+//  Created by Armen Phelps on 10/18/20.
+//
+
+import UIKit
+
+class ViewController5_feedback: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Transfer Assistant App/ViewController7_map.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController7_map.swift
+//  Transfer Assistant App
+//
+//  Created by Armen Phelps on 10/18/20.
+//
+
+import UIKit
+
+class ViewController7_map: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
