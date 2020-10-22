802d2f972d1146c4ed7e7247b4438f905c15f947
Jeremy Liu <jeremyliu@berkeley.edu>
2020-10-21 21:12:39 -0700

    Created xib file and custom table view cells

--- Transfer Assistant App/ViewController6_list.swift
+++ Transfer Assistant App/ViewController6_list.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 
-class ViewController6_list: UIViewController {
-
+class ViewController6_list: UIViewController, UITableViewDataSource {
+    
+    let course_name = ["CS61A", "CS61B", "CS61C", "EECS16A", "EE16B"]
+    @IBOutlet weak var tableViewCourse: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return course_name.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "course", for: indexPath)
+        
+        cell.textLabel?.text = course_name[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let course = course_name[indexPath.row]
+        print(course)
+      
+    }
 
     /*
     // MARK: - Navigation
