13507306abd268b63bfe8fe77753d0dcb63785b1
alextakahashi <alex.takahashi7@gmail.com>
2020-10-25 18:06:18 -0700

    Moved Custom View Files to App Directory

---
+++ Transfer Assistant App/Transfer Assistant App/ViewController2_home.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController2_home.swift
+//  Transfer Assistant App
+//
+//  Created by Armen Phelps on 10/18/20.
+//
+
+import UIKit
+
+class ViewController2_home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goToProfile(_ sender: Any) {
+    }
+    
+    @IBAction func goToSchedule(_ sender: Any) {
+    }
+    
+    @IBAction func goToList(_ sender: Any) {
+    }
+    
+    @IBAction func goToMap(_ sender: Any) {
+    }
+    
+    @IBAction func goToTips(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Transfer Assistant App/Transfer Assistant App/ViewController3_profile.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController3_profile.swift
+//  Transfer Assistant App
+//
+//  Created by Armen Phelps on 10/18/20.
+//
+
+import UIKit
+
+class ViewController3_profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func makeSchedule(_ sender: Any) {
+    }
+    
+    @IBAction func goToSchedule(_ sender: Any) {
+    }
+    
+    @IBAction func goHome(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
