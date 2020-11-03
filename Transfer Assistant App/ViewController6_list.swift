19974720e52b45c01d257cabdce6bee8f5419378
Jeremy Liu <jeremyliu@berkeley.edu>
2020-11-02 18:43:47 -0800

    cleaning up

--- Transfer Assistant App/ViewController6_list.swift
+++ Transfer Assistant App/ViewController6_list.swift
@@ -9,61 +9,37 @@ import UIKit
 
 class ViewController6_list: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let course_name = ["CS61A", "CS61B", "CS61C", "EECS16A", "EE16B"]
-    let course_description = ["intro 1", "intro 2", "intro 3", "intro 4", "intro 5"]
-    var selectedIndex = Int()
+    let courseName = ["CS61A", "CS61B", "CS61C", "EECS16A", "EE16B"]
+    let courseDescription = ["intro 1", "intro 2", "intro 3", "intro 4", "intro 5"]
+ 
     @IBOutlet weak var tableViewCourse: UITableView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-        // Do any additional setup after loading the view.
-    }
    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return course_name.count
+        return courseName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "course", for: indexPath)
         
-        cell.textLabel?.text = course_name[indexPath.row]
+        cell.textLabel?.text = courseName[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(" \(#function)")
-         selectedIndex = indexPath.row
-       
-        performSegue(withIdentifier: "description", sender: self)
-      
-    }
 
     
     override func prepare( for segue: UIStoryboardSegue, sender: Any?){
-        print("✅ \(#function)")
         
         if segue.identifier == "description"{
             
             if let indexPath = tableViewCourse.indexPathForSelectedRow{
                 let selectedRow = indexPath.row
                     let vc : CourseDescriptionViewController = segue.destination as! CourseDescriptionViewController
-                    print(course_description[selectedIndex])
-                    vc.name = course_description[selectedRow]
+                  
+                    vc.name = courseDescription[selectedRow]
             }
         }
     
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
