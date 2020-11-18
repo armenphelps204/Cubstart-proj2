7f258569ccea806d614583c870b7e38e1cb8767e
Armen Phelps <armenphelps@Armens-MacBook-Air.local>
2020-11-17 23:48:35 -0800

    made app demo video

--- Transfer Assistant App/Transfer Assistant App/ViewController.swift
+++ Transfer Assistant App/Transfer Assistant App/ViewController.swift
@@ -27,3 +27,9 @@ class ViewController: UIViewController {
     
 }
 
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
