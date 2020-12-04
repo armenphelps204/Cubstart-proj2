15406929b836b654618d6173cc9a954e994f9194
Jeremy Liu <jeremyliu@berkeley.edu>
2020-12-03 18:11:51 -0800

    add tips view

--- Transfer Assistant App/ViewController8_tips.swift
+++ Transfer Assistant App/ViewController8_tips.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController8_tips: UIViewController {
 
+    @IBOutlet weak var stackTips: UIStackView!
+    
+    @IBOutlet weak var tip1: UILabel!
+    @IBOutlet weak var tip2: UILabel!
+    
+    @IBOutlet weak var tip3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tip1.numberOfLines = 0
+        self.tip1.text = "Be willing to collaborate. You tend to make more friends and study partners when you are the one initiate the conversation."
+        tip2.numberOfLines = 0
+        self.tip2.text = "Take classes you are interested in and not what everyone else is doing. Only you know yourself best."
+        tip3.numberOfLines = 0
+        self.tip3.text = "Study more than you party."
         // Do any additional setup after loading the view.
     }
     
